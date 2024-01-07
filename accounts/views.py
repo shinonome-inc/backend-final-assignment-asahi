4803ae52894eeb96134cf27af0462a0e259351a6
asahi <ojimaasahi1018@gmail.com>
2024-01-07 15:39:47 +0900

    ✨ ログインログアウト機能の実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,16 @@
+from django.conf import settings
 from django.contrib.auth import authenticate, login
-
-# from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 
 from .forms import SignupForm
+
+# from django.shortcuts import render
 
 
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy("tweets:home")
+    success_url = settings.LOGIN_REDIRECT_URL
 
     def form_valid(self, form):
         # signupしたらそのままloginするように実装
@@ -20,3 +20,7 @@ class SignupView(CreateView):
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
+
+
+class UserProfileView(TemplateView):
+    template_name = "accounts/profile.html"
--- mysite/settings.py
+++ mysite/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Redirect URL after Login, Logout
+LOGIN_REDIRECT_URL = "/accounts/user_profile/"
+
+LOGOUT_REDIRECT_URL = "/accounts/login/"
