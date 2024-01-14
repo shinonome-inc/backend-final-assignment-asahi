69753bde39b58c78ef921c6d3eb662bba8970994
asahi <ojimaasahi1018@gmail.com>
2024-01-14 09:55:13 +0900

    🔨 モデルの修正

--- tweets/models.py
+++ tweets/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Tweet(models.Model):
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
-    content = models.TextField()
+    content = models.TextField(max_length=140)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
