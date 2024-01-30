fd9fda13d7ce539f5b0c542987af20d6008565ce
asahi <ojimaasahi1018@gmail.com>
2024-01-30 17:04:08 +0900

    🔨 ツイート削除機能の修正

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, ListView
 
@@ -22,7 +23,7 @@ class TweetCreateView(LoginRequiredMixin, CreateView):
     model = Tweet
     form_class = TweetForm
     template_name = "tweets/create.html"
-    success_url = reverse_lazy("tweets:home")
+    success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -35,7 +36,11 @@ class TweetDetailView(LoginRequiredMixin, DetailView):
     context_object_name = "tweet"
 
 
-class TweetDeleteView(LoginRequiredMixin, DeleteView):
+class TweetDeleteView(UserPassesTestMixin, DeleteView):
     model = Tweet
     template_name = "tweets/delete.html"
-    success_url = reverse_lazy("tweets:home")
+    success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)
+
+    def test_func(self):
+        object = self.get_object()
+        return object.user == self.request.user
