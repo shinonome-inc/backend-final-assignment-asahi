103d306429cb94d0df0278dfa2dc901e0c52e04c
asahi <ojimaasahi1018@gmail.com>
2024-02-10 15:53:06 +0900

    🔥 不要なコードの削除

--- tweets/forms.py
+++ tweets/forms.py
@@ -1,9 +1,6 @@
-from django.contrib.auth import get_user_model
 from django.forms import ModelForm
 
 from tweets.models import Tweet
-
-User = get_user_model()
 
 
 class CreateTweetForm(ModelForm):
