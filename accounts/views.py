ca9e0d01f5d90a3e3c120721704c200517c133a7
asahi <ojimaasahi1018@gmail.com>
2024-02-11 11:42:19 +0900

    🔨 エラーハンドリングの修正

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,11 @@ from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, TemplateView
 
 from accounts.models import FriendShip, User
 from tweets.models import Tweet
@@ -33,7 +33,7 @@ class CustomLoginView(LoginView):
     template_name = "accounts/login.html"
 
 
-class UserProfileView(LoginRequiredMixin, ListView):
+class UserProfileView(LoginRequiredMixin, TemplateView):
     template_name = "accounts/profile.html"
 
     def get_context_data(self, username):
@@ -46,13 +46,14 @@ class UserProfileView(LoginRequiredMixin, ListView):
         context["tweets"] = Tweet.objects.select_related("user").filter(user=user)
         return context
 
-class FollowView(LoginRequiredMixin,View):
 
-    def post(self,request,username):
-        following_user = get_object_or_404(User,username=username)
+class FollowView(LoginRequiredMixin, View):
+
+    def post(self, request, username):
+        following_user = get_object_or_404(User, username=username)
         # 自分自身をフォローしようとしている場合、プロフィール画面にリダイレクト
         if request.user == following_user:
-            return HttpResponseRedirect(reverse_lazy("accounts:user_profile", kwargs={"username": username}))
+            return HttpResponseBadRequest()
         # 既にフォローしているかチェック
         is_following = FriendShip.objects.filter(follower=request.user, following=following_user).exists()
         # 既にフォローしている場合、プロフィール画面にリダイレクト
@@ -65,14 +66,14 @@ class FollowView(LoginRequiredMixin,View):
         return HttpResponseRedirect(reverse_lazy(settings.LOGIN_REDIRECT_URL))
 
 
-class UnFollowView(LoginRequiredMixin,View):
+class UnFollowView(LoginRequiredMixin, View):
 
-    def post(self,request,username):
-        unfollowing_user = get_object_or_404(User,username=username)
+    def post(self, request, username):
+        unfollowing_user = get_object_or_404(User, username=username)
         follow_instance = FriendShip.objects.filter(follower=request.user, following=unfollowing_user)
         # 自分自身をフォローしようとしている場合、プロフィール画面にリダイレクト
         if request.user == unfollowing_user:
-            return HttpResponseRedirect(reverse_lazy("accounts:user_profile", kwargs={"username": username}))
+            return HttpResponseBadRequest()
 
         # フォローしていなければリダイレクト、していればフォロー解除
         if not follow_instance:
@@ -80,22 +81,32 @@ class UnFollowView(LoginRequiredMixin,View):
         else:
             follow_instance.delete()
 
-        return HttpResponseRedirect(reverse_lazy("accounts:user_profile", kwargs={"username": username}))
+        return HttpResponseRedirect(reverse_lazy("tweets:home"))
 
 
-class FollowingListView(LoginRequiredMixin,ListView):
+class FollowingListView(LoginRequiredMixin, ListView):
     model = FriendShip
     template_name = "accounts/followingList.html"
     context_object_name = "friendships"
 
     def get_queryset(self):
-        return FriendShip.objects.all().filter(follower=self.request.user).select_related("following").order_by("-created_at")
+        return (
+            FriendShip.objects.all()
+            .filter(follower=self.request.user)
+            .select_related("following")
+            .order_by("-created_at")
+        )
 
 
-class FollowerListView(LoginRequiredMixin,ListView):
+class FollowerListView(LoginRequiredMixin, ListView):
     model = FriendShip
     template_name = "accounts/followerList.html"
     context_object_name = "friendships"
 
     def get_queryset(self):
-        return FriendShip.objects.all().filter(following=self.request.user).select_related("follower").order_by("-created_at")
+        return (
+            FriendShip.objects.all()
+            .filter(following=self.request.user)
+            .select_related("follower")
+            .order_by("-created_at")
+        )
