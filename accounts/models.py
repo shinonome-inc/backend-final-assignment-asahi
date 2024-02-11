165b669d7a7eac752a35b86d7c2a07c8fbc00593
asahi <ojimaasahi1018@gmail.com>
2024-02-11 13:50:19 +0900

    🔧 管理画面の表示修正

--- accounts/models.py
+++ accounts/models.py
@@ -10,3 +10,6 @@ class FriendShip(models.Model):
     follower = models.ForeignKey(User, related_name="follower", on_delete=models.CASCADE)
     following = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.follower} → {self.following} ({self.created_at})"
