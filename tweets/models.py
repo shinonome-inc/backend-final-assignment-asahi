44d5ff23a5a855130799f1b9cf29dbc279fb1a0e
asahi <ojimaasahi1018@gmail.com>
2024-02-13 16:51:19 +0900

    🔨 Likeモデルの追加

--- tweets/models.py
+++ tweets/models.py
@@ -8,3 +8,17 @@ class Tweet(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.content} ({self.created_at})"
+
+
+class Like(models.Model):
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, related_name="likes", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} → {self.tweet} ({self.created_at})"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["user", "tweet"], name="like_unique"),
+        ]
